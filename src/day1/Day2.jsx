c66f459adea4402c3b27f16b9f9eb215701c6bcc
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2024-02-05 14:27:37 +0530

    Update Day2.jsx

--- src/day1/Day2.jsx
+++ src/day1/Day2.jsx
@@ -4,6 +4,11 @@ const Day2 = () => {
 
     const callbackhell = 'https://onecompiler.com/javascript/423g6u3vy';
 
+    function test1(data,callback){
+        const newData = data.map((ele)=> ele *2);
+        callback(newData)
+    }
+
     return (
         <div>Day2</div>
     )
