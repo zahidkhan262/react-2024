f37a0c6f34244ac4834f56e4c6a80641d3e53fba
zahidkhan <zahidkhan007860@mail.com>
2024-02-05 14:11:19 +0530

    add day2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Day1 from './day1/Day1';
+import Day2 from './day1/Day2';
 
 function App() {
   return (
     <>
-      <Day1 />
+      <Day2 />
     </>
   );
 }
---
+++ src/day1/Day2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Day2 = () => {
+
+    const callbackhell = 'https://onecompiler.com/javascript/423g6u3vy';
+
+    return (
+        <div>Day2</div>
+    )
+}
+
+export default Day2
